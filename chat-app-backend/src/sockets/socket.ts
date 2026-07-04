a52a3ba3c8ee059efa456deb691dd8530ac83721
harshillvashisht <harshillvashisht58@gmail.com>
2026-07-04 13:31:39 +0530

    forgot socket url and readded now for deployement

--- chat-app-backend/src/sockets/socket.ts
+++ chat-app-backend/src/sockets/socket.ts
@@ -8,7 +8,7 @@ let io: Server;
 export const initializeSocket = (server: http.Server) => {
     io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: process.env.CLIENT_URL,
         credentials: true,
     },
 });
